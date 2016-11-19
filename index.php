d04a7ee84f62c18846b9c04178843d4efbbbab03
Hitendra Rohilla <rohilla_hitendra@network.lilly.com>
2016-11-18 21:09:33 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$db_host = getenv('DB_HOST');
+$db_name = getenv('DB_NAME');
+$db_pass = getenv('DB_PASS');
+$db_user = getenv('DB_USER');
+# debug
+#echo "database host: $db_host <br>";
+#echo "database name: $db_name <br>";
+#echo "<br><br>";
+#connection to the database
+$dbhandle = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($dbhandle->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $dbhandle->connect_errno . ") " . $dbhandle->connect_error;
+    exit;
+}
+# debug info
+#echo $dbhandle->host_info . "\n";
+#Execute Query
+$result = $dbhandle->query("SELECT id, name, class, government FROM starships");
+echo "<H1>Star Trek Starships v1.3. Welcome OSE Team!!!</H1>";
+#Display data
+while ($row = $result->fetch_assoc()) {
+   echo "<b>Row:</b> ".$row{'id'}."<br><b>Name:</b> ".$row{'name'}."<br><b>Class:</b> ".$row{'class'}."<br><b>Government:</b> ".$row{'government'}."<br><br>";
+}
+#close the connection
+mysqli_close ( $dbhandle );
+?>
